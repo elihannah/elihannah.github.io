438e2809c5b469cb2e94cec2e08046eca92ee80a
esleland <eli@voltaiq.io>
2015-10-17 14:57:50 -0400

    Added detail on shuttles and attire

--- js/elihannah.js
+++ js/elihannah.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $("#shuttlelink").click(function(){
-        // $('.nav a[href="#shuttles"]').tab('show')
+    $(".shuttlelink").click(function(){
         $('.nav a[href="#shuttles"]').click()
     });
 });
